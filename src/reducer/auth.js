d9811825a5f99d6210d62b4bd945ec1620070af3
sy1lk <sy1lk@vn.ibm.com>
2021-03-07 20:05:38 +0700

    remove redundant code

--- src/reducer/auth.js
+++ src/reducer/auth.js
@@ -1,16 +1,9 @@
 import { actionType } from '../action/actionType';
 
 export const auth = (state = {
-    loadingCount: 0,
     searching: false
 }, action) => {
     switch(action.type) {
-        case actionType.CHANGE_LOADING_COUNT: {
-            return {
-                ...state,
-                loadingCount: state.loadingCount + 1
-            }
-        }
         case actionType.SEARCH_STUDENT: {
             return {
                 ...state,
